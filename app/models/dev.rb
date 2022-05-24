e272aa61c54fccec26ea1a35705f384ff846d976
tannerleroy <tanner@tleroy.com>
2022-05-24 10:55:19 -0400

    commit tree f03f87cfbc28d28a1d04f337bbd0e0ae0fac4bbd

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,18 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+
+    def received_one? (item_name)
+        self.freebies.any? do |freebie|
+        freebie.item_name == item_name
+       end
+    end 
+
+    def give_away (dev, freebie)
+        if self.id == freebie.dev_id
+            freebie.change(dev_id: dev.id)
+         end
+    end
 
 end
